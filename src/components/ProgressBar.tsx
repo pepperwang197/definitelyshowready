ad1f50b02c9dc683156d85493a594e10c4b3af4c
Pepper Wang <pepperwang197@gmail.com>
2026-07-14 22:23:41 -0700

    finished dark mode, added pointer cursor to some stuff

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -20,7 +20,7 @@ export default function ProgressBar(props: progressBarProps) {
   return (
     <>
       <Slider.Root
-        className="group relative flex items-center w-full h-4"
+        className="group relative flex items-center w-full h-4 cursor-pointer"
         onValueChange={(value: number[]) => {
           if (!moving) {
             setMoving(true);
@@ -35,7 +35,7 @@ export default function ProgressBar(props: progressBarProps) {
         max={props.duration}
         step={1}
       >
-        <Slider.Track className="relative grow size-full overflow-hidden rounded-full bg-slate-300">
+        <Slider.Track className="relative grow size-full overflow-hidden rounded-full bg-slate-300 dark:bg-gray-600">
           <Slider.Range className="absolute grow h-full bg-cyan-500" />
         </Slider.Track>
         <Slider.Thumb
